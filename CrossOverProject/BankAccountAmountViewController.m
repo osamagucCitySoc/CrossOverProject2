b05f41749c03b4ac2803819fb71a581c77c88e52
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2016-02-15 17:43:23 +0300

    1- Added the logic for updating the bank account amount. If the users entered something it is stored, if not a simple animation tells him that the amount field is required.

--- CrossOverProject/BankAccountAmountViewController.m
+++ CrossOverProject/BankAccountAmountViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BankAccountAmountViewController.h"
+#import "Constants.h"
 
 @interface BankAccountAmountViewController ()
 
@@ -28,7 +29,37 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)submitButtonClicked:(id)sender {
-    
+    // First, we need to check that the user has enetred something in the bank account amount textfield
+    if(amountTextField.text.length > 0)
+    {
+        // Then the user entered some number. Now we need to store it and get back to the caller UIViewController
+        NSNumber* enteredAmount;
+        if(minusPlusSegmentController.selectedSegmentIndex == 0)
+        {
+            // The user has a balance with positive value.
+            enteredAmount = [NSNumber numberWithDouble:[amountTextField.text doubleValue]];
+        }else
+        {
+            // The user has a balance with negative value.
+             enteredAmount = [NSNumber numberWithDouble:(-[amountTextField.text doubleValue])];
+        }
+        [[NSUserDefaults standardUserDefaults]setObject:enteredAmount forKey:consBankAccountUserDefaultsKey];
+        [[NSUserDefaults standardUserDefaults]synchronize];
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }else
+    {
+        // Then the user didn't enter anything, and we need to highlight that he should!
+        CABasicAnimation *animation =
+        [CABasicAnimation animationWithKeyPath:@"position"];
+        [animation setDuration:0.10];
+        [animation setRepeatCount:4];
+        [animation setAutoreverses:YES];
+        [animation setFromValue:[NSValue valueWithCGPoint:
+                                 CGPointMake([amountTextField center].x - 20.0f, [amountTextField center].y)]];
+        [animation setToValue:[NSValue valueWithCGPoint:
+                               CGPointMake([amountTextField center].x + 20.0f, [amountTextField center].y)]];
+        [[amountTextField layer] addAnimation:animation forKey:@"position"];
+    }
 }
 
 /*
