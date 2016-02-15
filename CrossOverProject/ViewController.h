73415600ee2b28cb336ba8da3791023e31534bbe
osamagucCitySoc <osamarabie@osamas-macbook-pro.local>
2016-02-15 16:50:23 +0300

    Initial Commit

---
+++ CrossOverProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrossOverProject
+//
+//  Created by Osama Rabie on 2/15/16.
+//  Copyright © 2016 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrossOverProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CrossOverProject
+//
+//  Created by Osama Rabie on 2/15/16.
+//  Copyright © 2016 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
